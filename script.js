37236d9506ae71729a2c021d1fad10996645b816
Pedro Fonseca <pmlfonseca@outlook.com>
2021-12-16 20:51:32 +0000

    First step to dark theme

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 
 // prints a message in the browser's dev tools console
 console.log("Hello 🌎");
+
+let isDarkTheme = false;
+
+function toggleDarkTheme() {
+	if (isDarkTheme) {
+	} else {
+		document.querySelector("body").style.backgroundColor = "#333333";
+
+		let textToChangeColor = document.querySelectorAll(
+			".title, .intro div p, .heading, .project-card-content h3, .project-card-content p"
+		);
+
+		for (let i = 0; i < textToChangeColor.length; i++) {
+			textToChangeColor[i].style.color = "#f9faff";
+		}
+	}
+}
+
+toggleDarkTheme();
